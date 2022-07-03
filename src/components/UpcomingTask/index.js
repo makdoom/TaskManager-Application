46bc2e4fd9d020901fe5f53c6dae57c27991d976
Makdoom Shaikh <makshaikh99@gmail.com>
2022-07-03 20:05:07 +0530

    added upcoming task sections

--- src/components/UpcomingTask/index.js
+++ src/components/UpcomingTask/index.js
@@ -1,11 +1,42 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./styles";
+import {
+  collection,
+  onSnapshot,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
+import TaskList from "../TaskList";
+import { db } from "../../config/firebase";
 
 const UpcomingTask = () => {
+  const [taskList, setTaskList] = useState([]);
+
+  useEffect(() => {
+    // Fetch upcoming task from firestore
+    const unsubscribe = onSnapshot(
+      query(
+        collection(db, "Tasks"),
+        where("taskCategory", "==", "upcoming"),
+        orderBy("timestamp", "desc")
+      ),
+      (snapshot) => {
+        setTaskList(snapshot.docs);
+      }
+    );
+
+    return unsubscribe;
+  }, []);
+
   return (
     <View>
-      <Text>UpcomingTask</Text>
+      {taskList.length > 0 ? (
+        <TaskList taskList={taskList} />
+      ) : (
+        <Text>No task yet</Text>
+      )}
     </View>
   );
 };
